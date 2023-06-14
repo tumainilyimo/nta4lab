2c435ff0cf82ab7722cdbc39d16fd638f421d226
tumainilyimo <lyimotumaini@gmail.com>
2023-06-14 13:59:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-import streamlit as st
-st.write("THIS IS MY NEW APP")
+number = random.randint(1, 100)
+
+guess = st.number_input("Enter a number (between 1 and 100):", min_value=1,max_value=100)
+
+if st.button('Make a guess!'):
+    if guess > number:
+        st.write('Too high! Try a smaller number.')
+    elif guess < number:
+        st.write('Too low! Try a larger number.')
+    else:
+        st.write('Congratulations! You have guessed the number correctly.')
